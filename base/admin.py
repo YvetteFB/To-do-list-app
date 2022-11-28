6ad4f14e897590e48b1457e90b4f5e927cf2c431
YvetteFB <fionabaraza@authel.io>
2022-11-28 12:24:15 +0300

    add filter

--- base/admin.py
+++ base/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Task
 
-admin.site.register(Task)
+# admin.site.register(Task)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_complete', 'created')
+    list_filter = ['is_complete']
+    actions = ['completed']
+
+    def completed(self, request, queryset):
+        queryset.update(is_complete=True)
+
