bcbc51e716129313243ce1da2b6a9e2fa9a26ebd
YvetteFB <fionabaraza@authel.io>
2023-01-14 14:40:11 +0300

    set up postgreSQL

--- Todo_List/settings.py
+++ Todo_List/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'Todo_List.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+          'ENGINE': 'django.db.backends.postgresql_psycopg2',
+          'NAME': 'todolist',
+          'USER': 'admin',
+          'PASSWORD': '1234',
+          'HOST': 'localhost',
+          'PORT': '',
+      }
 }
 
 # Password validation
