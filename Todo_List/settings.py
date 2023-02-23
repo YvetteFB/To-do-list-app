5bd3164acbcdad7648717878da5f3924301a3791
Fiona Yvette Baraza <fionabaraza@gmail.com>
2023-02-23 12:59:21 +0300

    change db

--- Todo_List/settings.py
+++ Todo_List/settings.py
@@ -123,5 +123,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-CSRF_TRUSTED_ORIGINS = ['.vercel.app']
+CSRF_TRUSTED_ORIGINS = ['https://*.vercel.app']
 
